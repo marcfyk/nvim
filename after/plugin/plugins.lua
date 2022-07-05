84f7754bbaf657442ce699c74ed60433e0c2bd1e
marcfyk <marcjohnfong@gmail.com>
2022-07-05 11:30:08 +0800

    Add config files

---
+++ after/plugin/plugins.lua
@@ -0,0 +1,56 @@
+require'packer'.startup(function()
+  -- Package Manager
+  use 'wbthomason/packer.nvim'
+
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+
+  -- Autocomplete
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+
+  -- Status Bar
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  -- Fuzzy Finder and File explorer
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'nvim-telescope/telescope-file-browser.nvim'
+
+  -- Git
+  use 'tpope/vim-fugitive'
+  use 'mhinz/vim-signify'
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require'gitsigns'.setup()
+    end
+  }
+
+  -- Themes
+  use { 'ayu-theme/ayu-vim', opt = true }
+  use { 'morhetz/gruvbox', opt = true }
+  use { 'arcticicestudio/nord-vim', opt = true }
+  use { 'NLKNguyen/papercolor-theme', opt = true }
+  use { 'altercation/vim-colors-solarized', opt = true }
+  use { 'junegunn/seoul256.vim', opt = true }
+
+end)
---
+++ init.lua
@@ -0,0 +1,22 @@
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+
+vim.o.cursorline = true
+
+vim.o.errorbells = false
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.hidden = true
+
+vim.o.relativenumber = true
+vim.o.number = true
+
+vim.o.incsearch = true
+
+vim.o.termguicolors = true
+vim.cmd[[colorscheme seoul256]]
+vim.o.background = 'light'
+
