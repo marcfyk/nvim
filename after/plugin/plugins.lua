d389a64c25de712d39137d1cf1834a01dddfb3a9
Marc <marcjohnfong@gmail.com>
2022-07-14 10:48:17 +0800

    Update plugins.lua

--- after/plugin/plugins.lua
+++ after/plugin/plugins.lua
@@ -25,16 +25,7 @@ require'packer'.startup(function()
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-    
-  -- Tree File Explorer
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
+  } 
 
   -- Fuzzy Finder and File Explorer
   use {
