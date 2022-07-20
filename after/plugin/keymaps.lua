d49d84124f77b32327f52ff78709b7d71a5b84e6
marcfyk <marcjohnfong@gmail.com>
2022-07-21 00:47:21 +0800

    Add keymaps and start banner

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,6 +1,9 @@
 -- Telescope
-vim.keymap.set("n", "<leader>b", "<cmd>Telescope find_files<CR>")
-vim.keymap.set("n", "<leader>f", "<cmd>Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "<leader>F", "<cmd>Telescope live_grep<CR>")
 
 -- NvimTree
 vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<CR>")
+
+-- Trouble
+vim.keymap.set("n", "<leader>D", "<cmd>TroubleToggle<CR>")
